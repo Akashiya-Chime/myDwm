58521ab4dfb7ab2fd24ddef7e0df6ff6aa9956e2
Akashiya-Chime <akashiyachime@163.com>
2021-11-23 10:25:17 +0800

    add hide_vacant_tags

--- scripts/dwm-status-refresh.sh
+++ scripts/dwm-status-refresh.sh
@@ -138,14 +138,14 @@ export IDENTIFIER="unicode"
 #. "$DIR/dwmbar-functions/dwm_transmission.sh"
 #. "$DIR/dwmbar-functions/dwm_cmus.sh"
 #. "$DIR/dwmbar-functions/dwm_resources.sh"
-#. "$DIR/dwmbar-functions/dwm_battery.sh"
-#. "$DIR/dwmbar-functions/dwm_mail.sh"
-#. "$DIR/dwmbar-functions/dwm_backlight.sh"
+# . "$DIR/dwmbar-functions/dwm_battery.sh"
+# . "$DIR/dwmbar-functions/dwm_mail.sh"
+. "$DIR/dwmbar-functions/dwm_backlight.sh"
 . "$DIR/dwmbar-functions/dwm_alsa.sh"
 #. "$DIR/dwmbar-functions/dwm_pulse.sh"
-#. "$DIR/dwmbar-functions/dwm_weather.sh"
+# . "$DIR/dwmbar-functions/dwm_weather.sh"
 #. "$DIR/dwmbar-functions/dwm_vpn.sh"
-#. "$DIR/dwmbar-functions/dwm_network.sh"
+# . "$DIR/dwmbar-functions/dwm_network.sh"
 #. "$DIR/dwmbar-functions/dwm_keyboard.sh"
 #. "$DIR/dwmbar-functions/dwm_ccurse.sh"
 # . "$DIR/dwmbar-functions/dwm_date.sh"
@@ -156,7 +156,7 @@ get_bytes
 vel_recv=$(get_velocity $received_bytes $old_received_bytes $now)
 vel_trans=$(get_velocity $transmitted_bytes $old_transmitted_bytes $now)
 
-xsetroot -name "  ⬇️ $vel_recv ⬆️ $vel_trans / $(dwm_alsa) / $(print_bat) / $(show_record) $(print_date) "
+xsetroot -name "  ⬇️ $vel_recv ⬆️ $vel_trans / $(dwm_backlight) / $(dwm_alsa) / $(print_bat) / $(show_record) $(print_date) "
 
 # Update old values to perform new calculations
 old_received_bytes=$received_bytes
